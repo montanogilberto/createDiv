4f24b36639dfe078c1ffa497bc84260d6f880d12
Gilberto Montaño <44681263+montanogilberto@users.noreply.github.com>
2018-12-12 23:58:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let arrayDiv = new Array();
+for(let i =o; i<= arrayDiv.length;i++){
+
+    arrayDiv[i] = document.createElement('div');
+    arrayDiv[i].id = 'div' + i;
+    //arrayDiv[i].className = '' 
+
+}
+
+/*
+document.body.appendChild(arrayDiv[i]);
+
+let idiv = document.createElement('');
+document.getElementsByName('body')[0].appendChild();
+*/
